cc8910386c71e9447a196b30e72f5bfadb179340
sanugiUP <113755041+sanugiUP@users.noreply.github.com>
2022-12-01 23:22:48 +0530

    Code For Authenticator

---
+++ Authenticator/AuthenticatorImplementation.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.ServiceModel;
+
+namespace Authenticator
+{
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Multiple, UseSynchronizationContext = false)]
+    internal class AuthenticatorImplementation : AuthenticatorInterface
+    {
+        private static Random rnd = new Random();
+
+        public int Login(string name, string password)
+        {
+            String tokenfilePath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName, "TokenData.txt");
+            String datafilePath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName, "RegistrationData.txt");
+
+            int token = 0;
+            String[] fileContents = File.ReadAllLines(datafilePath);
+            if (fileContents.Contains(name + " " + password))
+            {
+                token = rnd.Next(1, 100);
+                using (StreamWriter writer = new StreamWriter(tokenfilePath, true))
+                {
+                    writer.WriteLine(token);
+                    writer.Close();
+                }
+            }
+            return token;
+        }
+
+        public String Register(String name, String password)
+        {
+            String datafilePath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName, "RegistrationData.txt");
+            String regst_status;
+            String[] fileContents = File.ReadAllLines(datafilePath);
+
+            if(fileContents.Contains(name + " " + password))
+            {
+                regst_status = "Registration Unsuccessful!";
+            }
+            else
+            {
+                using (StreamWriter writer = new StreamWriter(datafilePath, true))
+                {
+                    writer.WriteLine(name + " " + password);
+                    writer.Close();
+                }
+                regst_status = "Successfully Registered";
+            } 
+            return regst_status;
+        }
+
+        public string Validate(int token)
+        {
+            String tokenfilePath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName, "TokenData.txt");
+            string valid_status;
+            String[] fileContents = File.ReadAllLines(tokenfilePath);
+
+            if (fileContents.Contains(token.ToString()))
+            {
+                valid_status = "Validated";
+            }
+            else
+            {
+                valid_status = "Not Validated";
+            }
+            return valid_status;
+        }
+    }
+}
+
+/* REF : https://www.c-sharpcorner.com/UploadFile/mahesh/how-to-read-a-text-file-in-C-Sharp/
+ * REF : https://stackoverflow.com/questions/5516870/how-to-write-data-to-a-text-file-without-overwriting-the-current-data
+ * REF : https://stackoverflow.com/questions/39970838/getting-a-directory-path-using-project-directory */
+
---
+++ Authenticator/AuthenticatorServer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.ServiceModel;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Authenticator
+{
+    public class AuthenticatorServer
+    {
+        public delegate void CleanUp(int num_of_mins);
+        private static int num_of_mins = 5; //Default Num Of Minutes To Clean Token File
+        private static ServiceHost host;
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To The Authentication Service!");
+            NetTcpBinding tcp = new NetTcpBinding();
+            host = new ServiceHost(typeof(AuthenticatorImplementation));
+            host.AddServiceEndpoint(typeof(AuthenticatorInterface), tcp, "net.tcp://localhost:8100/AuthenticationService");
+            host.Open();
+            Console.WriteLine("Authentication System Online");
+
+            Console.WriteLine("\n\nPlease Enter The Number Of Minutes For The Periodical Clean-Up Of Saved Tokens?");
+            string u_input = Console.ReadLine();
+            num_of_mins = int.Parse(u_input);
+
+            Task task = new Task(CleanTokens);
+            task.Start();
+
+            Console.ReadLine();
+            host.Close();
+        }
+
+        private static void CleanTokens()
+        {
+            while(host.State.ToString().Equals("Opened")) //Keep the method running on a seperate thread until server state changes
+            {
+                Thread.Sleep(num_of_mins * 10000); //Multiplied by 10000 to convert to minutes
+                File.WriteAllText(Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName, "TokenData.txt"), string.Empty);
+            }            
+        }
+    }
+}
+
+/* REF : https://stackoverflow.com/questions/4999988/clear-the-contents-of-a-file 
+ * REF : https://learn.microsoft.com/en-us/dotnet/api/system.servicemodel.communicationstate?view=netframework-4.8#system-servicemodel-communicationstate-opening */ 
